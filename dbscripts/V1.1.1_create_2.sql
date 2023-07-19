79c0d960f4bbc808d2c93584ea5ec7c17413eb9f
Taniya Tyagi <taniya.tyagi@hexaviewtech.com>
2023-07-19 16:51:53 +0530

    removed

--- dbscripts/V1.1.1_create_2.sql
+++
@@ -1,2 +0,0 @@
-use schema schema_test
-create table mycustomer_git_3(ID number,NAME varchar,AGE number ,ADDRESS varchar,SALARY decimal)
--- dbscripts/V1.1.2_create_3.sql
+++
@@ -1,2 +0,0 @@
-use schema SCHEMA_TEST;
-create table mycustomer_git_4(ID number,NAME varchar,AGE number ,ADDRESS varchar,SALARY decimal);
