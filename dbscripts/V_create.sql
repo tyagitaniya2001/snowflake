d1268b047da1cbe8f898b2d2cf464c6353c9e10b
Taniya Tyagi <taniya.tyagi@hexaviewtech.com>
2023-07-19 16:49:01 +0530

    new table 3

--- dbscripts/V_create.sql
+++
@@ -1,2 +0,0 @@
-use schema SCHEMA_TEST;
-create table mycustomer_git_2(ID number,NAME varchar,AGE number ,ADDRESS varchar,SALARY decimal);
--- dbscripts/alter.sql
+++
@@ -1,2 +0,0 @@
-use schema new_get_schema;
-alter table mycustomer_git add column gender varchar;
