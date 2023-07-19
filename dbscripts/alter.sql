4a4b4ed398f78ab5ca936480688aed56742fb33b
Taniya Tyagi <taniya.tyagi@hexaviewtech.com>
2023-07-19 15:42:12 +0530

    commiting scripts

---
+++ dbscripts/alter.sql
@@ -0,0 +1,2 @@
+use schema new_get_schema;
+alter table mycustomer_git add column gender varchar;
---
+++ dbscripts/create.sql
@@ -0,0 +1,2 @@
+create schema new_get_schema;
+create table mycustomer_git(ID number,NAME varchar,AGE number ,ADDRESS varchar,SALARY decimal);
